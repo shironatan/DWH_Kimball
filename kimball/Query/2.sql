18d7ea1777afe06d8eb99388072137838e074efa
root <467tn.mi@gmail.com>
2018-12-06 16:21:58 +0900

    naosita

--- kimball/Query/2.sql
+++ kimball/Query/2.sql
@@ -1,7 +1,7 @@
 use Kimball_DWH;
-SELECT Category.Category1_name AS 原作分類, Stories.Term_number AS クール, SUM(Sale_number) AS 作品数, AVG(Sale_number_count) AS 売上数値平均
+SELECT Category.Category1_name AS 原作分類, Official_tweet.Tweet AS ツイート数, SUM(Sale_number) AS 作品数, TRUNCATE(AVG(Sale_number_count),0) AS 売上数値平均
 FROM Sale
 INNER JOIN Category ON Sale.Category_key = Category.Category_key
-INNER JOIN Stories ON Sale.Stories_key = Stories.Stories_key
-GROUP BY 原作分類, クール
+INNER JOIN Official_tweet ON Sale.Official_tweet_key = Official_tweet.Official_tweet_key
+GROUP BY 原作分類, ツイート数
 ORDER BY 売上数値平均 desc;
--- kimball/Query/3.sql
+++ kimball/Query/3.sql
@@ -1,8 +1,8 @@
 use Kimball_DWH;
-SELECT Category.Category1_name AS 原作分類, Date.Term AS クール, Official_tweet.Tweet AS ツイート数, SUM(Sale_number) AS 作品数, AVG(Sale_number_count) AS 売上数値平均
+SELECT Category.Category1_name AS 原作分類, Date.Term AS クール, Stories.Term_number AS クール数, SUM(Sale_number) AS 作品数, TRUNCATE(AVG(Sale_number_count),0) AS 売上数値平均
 FROM Sale
 INNER JOIN Category ON Sale.Category_key = Category.Category_key
 INNER JOIN Date ON Sale.Date_key = Date.Date_key
-INNER JOIN Official_tweet ON Sale.Official_tweet_key = Official_tweet.Official_tweet_key
-GROUP BY 原作分類, クール, ツイート数
+INNER JOIN Stories ON Sale.Stories_key = Stories.Stories_key
+GROUP BY 原作分類, クール, クール数
 ORDER BY 売上数値平均 desc;
